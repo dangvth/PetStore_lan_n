fdb7508fbb2d2387c0e17798bb7868b3d17cc20b
Minh Tân <minhtanpn99@gmail.com>
2020-03-26 21:35:25 +0700

    Add method Insert Account and User

---
+++ PetStoreWebClient/Commom/AccountManagement.cs
@@ -0,0 +1,25 @@
+﻿
+using PetStoreWebClient.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetStoreWebClient.Commom
+{
+    public class AccountManagement
+    {
+        PetStoreOnlineDbContext db;
+        public AccountManagement()
+        {
+            db = new PetStoreOnlineDbContext();
+        }
+
+        public int InsertAccount(Account ac)
+        {
+            db.Account.Add(ac);
+            db.SaveChanges();
+            return ac.ac_id;
+        }
+    }
+}
---
+++ PetStoreWebClient/Commom/UserManagement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PetStoreWebClient.EF;
+
+namespace PetStoreWebClient.Commom
+{
+    public class UserManagement
+    {
+        PetStoreOnlineDbContext db;
+
+        public UserManagement()
+        {
+            db = new PetStoreOnlineDbContext();
+        }
+
+        public void InsertUser(string name, string gender, string email, string phone,
+            string address, int ac_id)
+        {
+            int userId = -1;
+            var user = new User();
+            user.u_name = name;
+            user.u_gender = gender;
+            user.u_email = email;
+            user.u_phone = phone;
+            user.u_address = address;
+            user.u_status = "Active";
+            user.ac_id = ac_id;
+
+            using (var db = new PetStoreOnlineDbContext())
+            {
+                db.User.Add(user);
+                db.SaveChanges();
+            }
+        }
+    }
+}
