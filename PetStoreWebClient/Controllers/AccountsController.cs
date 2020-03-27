d4854f78b2138fd76440f481024b03f36d495e01
Minh Tân <minhtanpn99@gmail.com>
2020-03-27 14:12:17 +0700

    Sign Up

--- PetStoreWebClient/Controllers/AccountsController.cs
+++ PetStoreWebClient/Controllers/AccountsController.cs
@@ -29,16 +29,12 @@ namespace PetStoreWebClient.Controllers
                 {
                     //Get the Account based on username has been inputted  
                     var account = am.getAccountByID(model.ac_userName);
-                    ////Initialize User Sesstion
-                    //var userSession = new UserSession();
-                    ////Assign username and account id into UserSession 
-                    //userSession.userName = account.ac_userName;
-                    //userSession.accountID = account.ac_id;
 
-                    ////Create session "USER_SESSION" contain username and account ID of account
-                    //Session.Add(CommonConstant.USER_SESSION, userSession);
+                    //Initialize User Management
                     UserManagement um = new UserManagement();
+                    //Get User by Account ID
                     var user = um.getUserByAccountID(account.ac_id);
+                    //Create Session to store username and ID of user
                     Session["username"] = account.ac_userName;
                     Session["userID"] = user;
 
@@ -61,9 +57,55 @@ namespace PetStoreWebClient.Controllers
             return View("Index");
         }
 
-        //public ActionResult Create()
-        //{
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View("Index");
+        }
 
-        //}
-    } 
+        public ActionResult Create(Account account, FormCollection Fields)
+        {
+            //declare variables and get value from Sign Up form
+            string fullname = Fields["fullName"];
+            string addr = Fields["address"];
+            string email = Fields["email"];
+            string phone = Fields["phone"];
+            string gender = Fields["gender"];
+            string username = account.ac_userName.ToString();
+            string password = account.ac_pwd.ToString();
+
+            //Check all fields has been fill
+            if (fullname.Equals("") || addr.Equals("") || email.Equals("") || phone.Equals("")
+                || username.Equals("") || password.Equals(""))
+            {
+                ModelState.AddModelError("", "Any fields can not be blank!!!!");
+                return View("Index");
+            }
+            else
+            {
+                //Assgin data into variables in Account
+                //var ac = new Account();
+                //ac.ac_userName = username;
+                //ac.ac_pwd = Encryptor.SHA256_Encrypt(account.ac_pwd).ToString();
+                //ac.ac_status = "Active";
+                //ac.r_id = 3;
+
+                //Insert Account and return account ID
+                AccountManagement am = new AccountManagement();
+                var pwdEncrypt = Encryptor.SHA256_Encrypt(account.ac_pwd);
+                account.ac_pwd = pwdEncrypt;
+                account.ac_status = "Active";
+                account.r_id = 3;
+                int acID = am.InsertAccount(account);
+                if (acID > 0)
+                {
+                    UserManagement um = new UserManagement();
+                    um.InsertUser(fullname, gender, email, phone, addr, acID);
+                    Session["username"] = account.ac_userName;
+                    //Session["userID"] = user;
+                }
+                return View("Index", "Home");
+            }
+        }
+    }
 }
