453b3263dbbc567852c2057851608543d357603e
Minh Tân <minhtanpn99@gmail.com>
2020-03-27 12:56:46 +0700

    Login

--- PetStoreWebClient/Commom/AccountManagement.cs
+++ PetStoreWebClient/Commom/AccountManagement.cs
@@ -9,6 +9,7 @@ namespace PetStoreWebClient.Commom
     public class AccountManagement
     {
         PetStoreOnlineDbContext db;
+
         public AccountManagement()
         {
             db = new PetStoreOnlineDbContext();
@@ -19,6 +20,80 @@ namespace PetStoreWebClient.Commom
             db.Account.Add(ac);
             db.SaveChanges();
             return ac.ac_id;
+        }
+
+        /// <summary>
+        /// get Account by username
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
+        public Account getAccountByID(string userName)
+        {
+            return db.Account.SingleOrDefault(x => x.ac_userName == userName);
+        }
+
+        /// <summary>
+        /// Check Username and password is correct
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public int Login(string userName, string password)
+        {
+            var result = db.Account.SingleOrDefault(x => x.ac_userName == userName);
+            if (result == null)
+            {
+                return -1;
+            }
+            else
+            {
+                if (result.ac_status == "Banned")
+                {
+                    return 0;
+                }
+                else
+                {
+                    if (result.ac_pwd == password)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -2;
+                    }
+                }
+            }
+        }
+
+        public bool isAccountExists(string username)
+        {
+            var account = db.Account.FirstOrDefault(x => x.ac_userName == username);
+            if (account != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Account Status must is "Active" or "Banned".
+        /// If difference them then send error message
+        /// </summary>
+        /// <param name="status"></param>
+        /// <returns></returns>
+        public bool isStatus(string status)
+        {
+            if (status.Equals("Active") || status.Equals("Banned"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- PetStoreWebClient/Controllers/AccountsController.cs
+++ PetStoreWebClient/Controllers/AccountsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PetStoreWebClient.EF;
+using PetStoreWebClient.Commom;
 
 namespace PetStoreWebClient.Controllers
 {
@@ -13,6 +14,48 @@ namespace PetStoreWebClient.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Login(Account model)
+        {
+            if (ModelState.IsValid)
+            {
+                AccountManagement am = new AccountManagement();
+                //Check user enter username & password is correct or not
+                int result = am.Login(model.ac_userName, model.ac_pwd);
+
+                //If is correct => get account and create session for that account
+                if (result == 1)
+                {
+                    //Get the Account based on username has been inputted  
+                    var account = am.getAccountByID(model.ac_userName);
+                    ////Initialize User Sesstion
+                    //var userSession = new UserSession();
+                    ////Assign username and account id into UserSession 
+                    //userSession.userName = account.ac_userName;
+                    //userSession.accountID = account.ac_id;
+
+                    ////Create session "USER_SESSION" contain username and account ID of account
+                    //Session.Add(CommonConstant.USER_SESSION, userSession);
+                    Session["username"] = account.ac_userName;
+
+                    //Redirect to action "Index" on "AccountsControllers"
+                    return RedirectToAction("Index", "Accounts");
+                }
+                else if (result == -1)  //Account is not exist
+                {
+                    ModelState.AddModelError("", "The username is not exist!!!");
+                }
+                else if (result == -2)  //Password is not correct
+                {
+                    ModelState.AddModelError("", "Password is not correct!!!");
+                }
+                else if (result == 0)    //Account has been banned
+                {
+                    ModelState.AddModelError("", "The username has been banned!!!");
+                }
+            }
+            return View("Index", "Home");
         }
 
         //public ActionResult Create()
