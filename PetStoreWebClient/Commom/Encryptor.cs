97669fabe1a1dfd56a367913ea02e9d8cb4bb5ec
Minh Tân <minhtanpn99@gmail.com>
2020-03-27 12:56:38 +0700

    Encrypt SHA256

---
+++ PetStoreWebClient/Commom/Encryptor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStoreWebClient.Commom
+{
+    public class Encryptor
+    {
+        public static string SHA256_Encrypt(string userInput)
+        {
+            //create a SHA256 encrypt
+            using (SHA256 sha256 = SHA256Managed.Create())
+            {
+                //ComputeHash - returns byte array
+                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(userInput));
+
+                //Convert byte array to a String
+                StringBuilder hashString = new StringBuilder();
+                foreach (byte i in hashBytes)
+                {
+                    hashString.Append(i.ToString("x2"));
+                }
+                return hashString.ToString();
+            }
+        }
+    }
+}
