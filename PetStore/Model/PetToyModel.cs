9169a3a60893280be89c253a959970f26c6e8438
Hải Đăng <dangvth1999@gmail.com>
2020-03-29 13:46:30 +0700

    Done accept bill

---
+++ PetStore/Model/PetToyModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.Model
+{
+    class PetToyModel
+    {
+        PetStoreEntities db = new PetStoreEntities();
+        public PetToyModel() {
+
+        }
+        public PetToy getPetToy(String pt_id)
+        {
+            var db = new PetStoreEntities();
+            var pt = db.PetToys.Find(pt_id);
+            return pt;
+        }
+    }
+}
