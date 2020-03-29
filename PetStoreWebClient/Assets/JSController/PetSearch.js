5ed8d8546d4eae37fdafea77acd3f130fcf8d7a2
Minh Tân <minhtanpn99@gmail.com>
2020-03-29 18:02:46 +0700

    Search Pet

---
+++ PetStoreWebClient/Assets/JSController/PetSearch.js
@@ -0,0 +1,36 @@
+﻿var common = {
+    init: function () {
+        common.registerEvent();
+    },
+    registerEvent: function () {
+        $("#txtKeywordPet").autocomplete({
+            minLength: 0,
+            source: function( request, response ) {
+                $.ajax( {
+                    url: "/Pet/ListName",
+                    dataType: "json",
+                    data: {
+                        p: request.term
+                    },
+                    success: function( res ) {
+                        response( res.data );
+                    }
+                });
+            },
+            focus: function (event, ui) {
+                $("#txtKeywordPet").val(ui.item.label);
+                return false;
+            },
+            select: function (event, ui) {
+                $("#txtKeywordPet").val(ui.item.label);
+                return false;
+            }
+        })
+        .autocomplete("instance")._renderItem = function (ul, item) {
+            return $("<li id='autocompleteItem'>")
+           .append("<div>" + item.label + "</div>")
+           .appendTo(ul);
+        };
+    }
+}
+common.init();
