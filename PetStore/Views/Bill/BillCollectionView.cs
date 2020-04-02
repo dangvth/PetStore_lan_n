4c6dbfb24400b20f1a752b07dc85bc19af8482d6
Hải Đăng <dangvth1999@gmail.com>
2020-04-02 15:58:34 +0700

    Done app

--- PetStore/Views/Bill/BillCollectionView.cs
+++ PetStore/Views/Bill/BillCollectionView.cs
@@ -6,9 +6,13 @@ using DevExpress.XtraGrid.Views.Grid;
 using DevExpress.XtraEditors;
 using DevExpress.Utils.MVVM.Services;
 using DevExpress.XtraBars;
+using System.Windows.Forms;
+using System.Data;
+using PetStore.Model;
 
 namespace PetStore.Views.BillCollectionView{
     public partial class BillCollectionView : XtraUserControl {
+        int selectedID;
         public BillCollectionView() {
             InitializeComponent();
 			if(!mvvmContext.IsDesignMode)
@@ -38,6 +42,77 @@ namespace PetStore.Views.BillCollectionView{
                     popupMenu.ShowPopup(gridControl.PointToScreen(e.Location), s);
                 }
             };
+        }
+
+        private void bbiDetail_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            DataGridView grvPrint = new DataGridView();
+            DataTable dt = new DataTable();
+            dt.Columns.Add(new DataColumn("Name", typeof(string)));
+            dt.Columns.Add(new DataColumn("Price", typeof(int)));
+            dt.Columns.Add(new DataColumn("Quantity", typeof(int)));
+            using (var db = new PetStoreEntities())
+            {
+                var list = db.BillDetails.Where(x => x.b_id == selectedID);
+                PetModel pm = new PetModel();
+                PetFoodModel pfm = new PetFoodModel();
+                PetMedicineModel pmm = new PetMedicineModel();
+                PetToyModel ptm = new PetToyModel();
+                PetAccessoryModel pam = new PetAccessoryModel();
+                int d = 0;
+                while (d != list.Count())
+                {
+                    var bd = list.ToList()[d];
+                    if (bd.p_id != null)
+                    {
+                        var p = pm.getPet(bd.p_id);
+                        var qty = list.Where(x => x.p_id == bd.p_id).Count();
+                        dt.Rows.Add(p.p_name, p.p_salePrice, qty);
+                        d += qty;
+                    }
+                    else if (bd.pt_id != null)
+                    {
+                        var pt = ptm.getPetToy(bd.pt_id);
+                        var qty = list.Where(x => x.pt_id == bd.pt_id).Count();
+                        dt.Rows.Add(pt.pt_name, pt.pt_salePrice, qty);
+                        d += qty;
+                    }
+                    else if (bd.pf_id != null)
+                    {
+                        var pf = pfm.getPetFood(bd.pf_id);
+                        var qty = list.Where(x => x.pf_id == bd.pf_id).Count();
+                        dt.Rows.Add(pf.pf_name, pf.pf_salePrice, qty);
+                        d += qty;
+                    }
+                    else if (bd.pm_id != null)
+                    {
+                        var pmd = pmm.getPetMedicine(bd.pm_id);
+                        var qty = list.Where(x => x.pm_id == bd.pm_id).Count();
+                        dt.Rows.Add(pmd.pm_name, pmd.pm_salePrice, qty);
+                        d += qty;
+                    }
+                    else if (bd.pa_id != null)
+                    {
+                        var pa = pam.getPetAccessory(bd.pa_id);
+                        var qty = list.Where(x => x.pa_id == bd.pa_id).Count();
+                        dt.Rows.Add(pa.pa_name, pa.pa_salePrice, qty);
+                        d += qty;
+                    }
+                }
+            }
+            DetailBill dbf = new DetailBill();
+            dbf.grvDetail.DataSource = dt;
+            dbf.ShowDialog(this);
+        }
+
+        private void gridView_RowClick(object sender, RowClickEventArgs e)
+        {
+            int idx = gridView.FocusedRowHandle;
+            selectedID = int.Parse(gridView.GetRowCellValue(idx, "b_id").ToString());
+            if (selectedID != -1)
+            {
+                bbiDetail.Enabled = true;
+            }
         }
     }
 }
